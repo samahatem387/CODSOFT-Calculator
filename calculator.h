b196f85de98b8edb757a989328cacdc272202f14
samahatem387 <116303447+samahatem387@users.noreply.github.com>
2024-05-19 23:34:42 +0300

    calculator.h

---
+++ calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    double add(double a, double b) const;
+    double subtract(double a, double b) const;
+    double multiply(double a, double b) const;
+    double divide(double a, double b) const;
+};
+
+#endif // CALCULATOR_H
